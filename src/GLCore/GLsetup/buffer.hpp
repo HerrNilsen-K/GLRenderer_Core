f60867c0608f3f6b4a2aa71b4370ae12b220d1d1
HerrNilsen-K <typnilsen@gmail.com>
2022-02-18 14:19:01 +0100

    Created the vbo core

--- src/GLCore/GLsetup/buffer.hpp
+++ src/GLCore/GLsetup/buffer.hpp
@@ -5,4 +5,28 @@
 #ifndef GLRENDERER_CORE_BUFFER_HPP
 #define GLRENDERER_CORE_BUFFER_HPP
 
+#include <GL/glew.h>
+
+namespace vbo {
+
+    inline GLuint create() {
+        GLuint buffer;
+        glCreateBuffers(1, &buffer);
+        return buffer;
+    }
+
+    inline void deleteBuffer(GLuint buffer) {
+        glDeleteBuffers(1, &buffer);
+    }
+
+    inline void bind(GLuint buffer) {
+        glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    }
+
+    //TODO: change void* data to modern c++
+    inline void data(GLuint buffer, GLsizei size, const void* data) {
+        glNamedBufferData(buffer, size, data, GL_STATIC_DRAW);
+    }
+
+}
 #endif //GLRENDERER_CORE_BUFFER_HPP
